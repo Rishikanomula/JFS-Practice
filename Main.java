f9f51e364344f6b2991523723717523286957dd2
Rishika <rishikanomula2006@gmail.com>
2025-10-06 10:48:38 +0530

    Pract_2(6/10/25)

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+//Interview Questions asked for our seniors 
 //Q1:
 
 /*class MCQ{
@@ -9,19 +10,62 @@
 
 //Q2:
 
-class Animal{
-    void makeSound(){
-        System.out.println("Animal makes a sound");
-    }
-}
-class Dog extends Animal{
-    void makeSound(){
-        System.out.println("Dog barks");
-    }
-}
-public class Main{
-    public static void main(String args[]){
-        Animal a = new Dog();
-        a.makeSound();
-    }
-}
+// class Animal{
+//     void makeSound(){
+//         System.out.println("Animal makes a sound");
+//     }
+// }
+// class Dog extends Animal{
+//     void makeSound(){
+//         System.out.println("Dog barks");
+//     }
+// }
+// public class Main{
+//     public static void main(String args[]){
+//         Animal a = new Dog();
+//         a.makeSound();
+//     }
+// }
+
+//Q3:
+
+// abstract class Parent{
+//     abstract void show(); //asbtract method should'nt have body (i.e they should'nt have any code within them no "{}" brackets)
+// }
+// class Child extends Parent{
+//     void show(){
+//         System.out.println("Child class method!");
+//     }
+// }
+// public class Main{
+//     public static void main(String[] args){
+//         Parent p = new Child();
+//         p.show();
+//     }
+// }
+
+
+//Q4:
+
+// abstract class demo{
+//     public int a;
+//     demo(){
+//         a=10;
+//     }
+//     abstract public void set();
+//     abstract public void get();
+// }
+// class Test extends demo{
+//     public void set(int a){
+//         this.a=a;
+//     }
+//     final public void get(){
+//         System.out.println("a = "+a);
+//     }
+//     public static void main(String[] args){
+//         Test obj = new Test();
+//         obj.set(20);
+//         obj.get();
+//     }
+// }
+
