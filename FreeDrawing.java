e063b5b30047c0547a82f69345382d5e8635a76b
Rishika <rishikanomula2006@gmail.com>
2025-10-27 10:31:33 +0530

    Free Draw Program Updated

--- FreeDrawing.java
+++ FreeDrawing.java
@@ -1,24 +1,67 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 
-public class FreeDrawing {
+public class FreeDrawing extends JFrame {
     int prevX, prevY; // last mouse position
     boolean drawing = false;
-    public FreeDrawing(){
-        setTitle("Free Grawing!");
-        setSize(600,400);
+    BufferedImage canvas;
+    Graphics2D canvasGraphics;
+
+    public FreeDrawing() {
+        setTitle("Free Drawing!");
+        setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel(){
+
+        canvas = new BufferedImage(600, 400, BufferedImage.TYPE_INT_ARGB);
+        canvasGraphics = canvas.createGraphics();
+        canvasGraphics.setColor(Color.BLACK);
+        canvasGraphics.setStroke(new BasicStroke(2));
+
+        JPanel panel = new JPanel() {
             @Override
-            protected void paintComponent(Graphics g){
+            protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
+                g.drawImage(canvas, 0, 0, null);
             }
         };
+
         panel.setBackground(Color.WHITE);
-        //mouse event registration
-        panel.addMouseListener(new MouseListener() {
-            
+
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                drawing = true;
+                prevX = e.getX();
+                prevY = e.getY();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                drawing = false;
+            }
         });
+
+        panel.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (drawing) {
+                    int x = e.getX();
+                    int y = e.getY();
+                    canvasGraphics.drawLine(prevX, prevY, x, y);
+                    prevX = x;
+                    prevY = y;
+                    panel.repaint();
+                }
+            }
+        });
+
+        add(panel);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(FreeDrawing::new);
     }
 }
